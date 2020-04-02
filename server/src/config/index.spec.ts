fba912bc83bac60dd2f3417c47a69be96c1a0426
Ahmad Tirmazi <ahmadtirmazi@yahoo.com>
2020-04-03 00:06:43 +0400

    Improve test config; Write unit tests for config;

---
+++ server/src/config/index.spec.ts
@@ -0,0 +1,10 @@
+describe("config", () => {
+  it("should export object with correct keys", () => {
+    const config = require("./").default;
+
+    expect(config).toMatchObject({
+      port: expect.any(Number),
+      baseApiUrl: expect.any(String)
+    });
+  });
+});
